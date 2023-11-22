280716171682560073d140f23b2588f92becf97e
EmmaHsieh <z0905631590@gmail.com>
2023-11-22 15:19:59 +0800

    更新內容

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 import 'dotenv/config'
 import linebot from 'linebot'
+
+const bot = linebot({
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+})
+
+bot.on('message', (event) => {
+  if (event.message.type === 'text') {
+    event.reply(event.message.text)
+  }
+})
+
+bot.listen('/', process.env.PORT || 3000, () => {
+  console.log('機器人啟動')
+})
