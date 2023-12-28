20c4abb4c436625d42410ae00120d4ea4c9ca2ed
EmmaHsieh <z0905631590@gmail.com>
2023-12-29 00:33:00 +0800

    新增快速回復紐

--- commands/quickReply.js
+++ commands/quickReply.js
@@ -40,8 +40,72 @@ export default async (event) => {
           type: 'action',
           action: {
             type: 'postback',
+            label: '宵夜',
+            data: `宵夜,${event.message.latitude},${event.message.longitude},${event.message.address}`
+          }
+        },
+        {
+          type: 'action',
+          action: {
+            type: 'postback',
             label: '小吃',
             data: `小吃,${event.message.latitude},${event.message.longitude},${event.message.address}`
+          }
+        },
+        {
+          type: 'action',
+          action: {
+            type: 'postback',
+            label: '火鍋',
+            data: `火鍋,${event.message.latitude},${event.message.longitude},${event.message.address}`
+          }
+        },
+        {
+          type: 'action',
+          action: {
+            type: 'postback',
+            label: '牛排',
+            data: `牛排,${event.message.latitude},${event.message.longitude},${event.message.address}`
+          }
+        },
+        {
+          type: 'action',
+          action: {
+            type: 'postback',
+            label: '日式料理',
+            data: `日式料理,${event.message.latitude},${event.message.longitude},${event.message.address}`
+          }
+        },
+        {
+          type: 'action',
+          action: {
+            type: 'postback',
+            label: '咖啡',
+            data: `咖啡,${event.message.latitude},${event.message.longitude},${event.message.address}`
+          }
+        },
+        {
+          type: 'action',
+          action: {
+            type: 'postback',
+            label: '甜點',
+            data: `甜點,${event.message.latitude},${event.message.longitude},${event.message.address}`
+          }
+        },
+        {
+          type: 'action',
+          action: {
+            type: 'postback',
+            label: '酒吧',
+            data: `酒吧,${event.message.latitude},${event.message.longitude},${event.message.address}`
+          }
+        },
+        {
+          type: 'action',
+          action: {
+            type: 'postback',
+            label: '早午餐',
+            data: `早午餐,${event.message.latitude},${event.message.longitude},${event.message.address}`
           }
         }
       ]
